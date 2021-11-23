62e6a7a4bbb79c6c64154324329eb79e0482171d
maxwu <maxwunj@gmail.com>
2021-11-23 23:58:29 +1300

    Add reverse UT

--- linkedlist/singlylinkedlist_test.go
+++ linkedlist/singlylinkedlist_test.go
@@ -5,11 +5,22 @@ import (
 	"testing"
 )
 
-func TestSinglylist(t *testing.T) {
+func TestSinglyLinkedList(t *testing.T) {
 	ar := []int32{0, 1, 2, 3, 4}
 	llist := SliceToSinglyLinkedList(ar)
 	actual := SinglyLinkedListToSlice(llist)
 	if !reflect.DeepEqual(ar, actual) {
+		t.Errorf("expect %v but actual value is %v", ar, actual)
+	}
+}
+
+func TestReverseSinglyLinkedList(t *testing.T) {
+	ar := []int32{0, 1, 2, 3, 4}
+	llist := SliceToSinglyLinkedList(ar)
+	reversed := Reverse(llist)
+	expected := []int32{4, 3, 2, 1, 0}
+	actual := SinglyLinkedListToSlice(reversed)
+	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("expect %v but actual value is %v", ar, actual)
 	}
 }
